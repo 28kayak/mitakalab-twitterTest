503c6a309fe84b2276c5eb32e539e1e3876e7c89
28kayak <kaya_ota@msn.com>
2014-10-31 19:25:30 -0700

    add method in UserController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
   def show
+    @user = Hash.new
+    @user[:name] = 'Shohei Aoki'
+    @user[:username] = 'moyahima'
+    @user[:location] = 'Tottori, Japan'
+    @user[:about] = 'Nice to meet you'
+  end
+  def list
   end
 
 
